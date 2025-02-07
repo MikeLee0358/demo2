8047ad3276afb1e2d48f21bf21ba6ecdc84880c7
Mike <cspkno005@gmail.com>
2025-02-07 18:31:47 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import Demo from '../components/Demo.vue'
+// background: #237CAB;
+// background: #C36659;
+
+
+// background: #237CAB;
+// background: #5BB7E7;
+// background: #46D688;
+// background: #2F9B34;
+// background: #38640C;
+</script>
+
+
+<template>
+  <main style="width: 880px; height: 40px; background-color: lightgray;">
+    <Demo style="width: 880px; height: 12px;" color="#237CAB" percent="100%" color2="#C36659" percent2="100%"/>
+  </main>
+</template>
+
