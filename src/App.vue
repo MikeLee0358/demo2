cdc21d9093e8cb91f9a5daf260df1871f244be20
mike <cspkno005@gmail.com>
2025-02-09 23:54:32 +0800

    feat: 🎸 done

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,11 @@
 <script setup lang="ts">
-import Demo from '../components/Demo.vue'
-// background: #237CAB;
-// background: #C36659;
-
-
-// background: #237CAB;
-// background: #5BB7E7;
-// background: #46D688;
-// background: #2F9B34;
-// background: #38640C;
+import ProgressBar from './components/ProgressBar.vue'
 </script>
 
 
 <template>
-  <main style="width: 880px; height: 40px; background-color: lightgray;">
-    <Demo style="width: 880px; height: 12px;" color="#237CAB" percent="100%" color2="#C36659" percent2="100%"/>
+  <main >
+    <ProgressBar percent="10" percent2="20" percent3="40" percent4="60" percent5="100" />
   </main>
 </template>
 
---
+++ src/components/ProgressBar.vue
@@ -0,0 +1,75 @@
+<script setup lang="ts">
+const props = defineProps<{
+  percent: string
+  percent2: string
+  percent3: string
+  percent4: string
+  percent5: string
+}>()
+
+</script>
+
+<template>
+  <section id="🔥ProgressBar">
+    <div id="🔥ProgressBar__Color"></div>
+    <div id="🔥ProgressBar__Color"></div>
+    <div id="🔥ProgressBar__Color"></div>
+    <div id="🔥ProgressBar__Color"></div>
+    <div id="🔥ProgressBar__Color"></div>
+  </section>
+</template>
+
+<style lang="scss">
+#🔥ProgressBar {
+  position: relative;
+  mask-image: url('../assets/progressbar.png');
+  mask-repeat: no-repeat;
+  width: 880px;
+  height: 12px;
+}
+
+#🔥ProgressBar__Color {
+  position: absolute;
+  width: 2000px;
+  height: inherit;
+
+  &::after {
+    content: '';
+    position: absolute;
+    left: 100%;
+    width: 2px;
+    height: inherit;
+    background-color: #000;
+  }
+}
+
+#🔥ProgressBar__Color:nth-of-type(1) {
+  z-index: 5;
+  width: calc(v-bind('props.percent') / 100 * 100%);
+  background: #237CAB;
+}
+
+#🔥ProgressBar__Color:nth-of-type(2) {
+  z-index: 4;
+  width: calc(v-bind('props.percent2') / 100 * 100%);
+  background: #5BB7E7;
+}
+
+#🔥ProgressBar__Color:nth-of-type(3) {
+  z-index: 3;
+  width: calc(v-bind('props.percent3') / 100 * 100%);
+  background: #46D688;
+}
+
+#🔥ProgressBar__Color:nth-of-type(4) {
+  z-index: 2;
+  width: calc(v-bind('props.percent4') / 100 * 100%);
+  background: #2F9B34;
+}
+
+#🔥ProgressBar__Color:nth-of-type(5) {
+  z-index: 1;
+  width: calc(v-bind('props.percent5') / 100 * 100%);
+  background: #38640C;
+}
+</style>
